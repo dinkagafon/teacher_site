4ee1aac79187ce5a9ccd2108442f6d60f7926ab3
Никита Агафонов <agafonikita@outlook.com>
2022-12-08 13:41:14 +0300

    start develop header

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const postcss = require("postcss");
 const postcssImport = require("postcss-import");
+const yaml = require("js-yaml");
 
 module.exports = function (eleventyConfig) {
   const rootStyle = "./src/styles/index.css";
@@ -33,6 +34,7 @@ module.exports = function (eleventyConfig) {
       output: "dist",
       includes: "includes",
       layouts: "layouts",
+      data: "data",
     },
     dataTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
