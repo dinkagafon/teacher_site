0cd2a967435ab1868e4511e6440543ec7a112348
Никита Агафонов <agafonikita@outlook.com>
2022-12-19 18:33:21 +0300

    add stages section

--- src/scripts/components/slider.js
+++ src/scripts/components/slider.js
@@ -1,94 +1,40 @@
-const [slider] = document.getElementsByClassName("slider");
+import Swiper, { Navigation, Pagination } from "swiper";
 
-const [prevButton, nextButton] =
-  slider.getElementsByClassName("slider__button");
+const swiper = new Swiper(".slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  modules: [Navigation],
+});
 
-const [elementsContainer] = slider.getElementsByClassName("slider__container");
+const expirienceStages = document.querySelectorAll(".stage");
 
-const elements = elementsContainer.getElementsByClassName("slider__elem");
-const halfContainerWidth = elementsContainer.offsetWidth / 2;
-const elementsPositions = Array.from(elements).map((sliderElement) => [
-  sliderElement.offsetLeft,
-  sliderElement.offsetLeft + sliderElement.offsetWidth,
-]);
+const swiper2 = new Swiper(".experience__slider", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  grabCursor: true,
+  initialSlide: 2,
+  pagination: {
+    el: ".experience__slider-pagination",
+  },
+  modules: [Pagination],
+  on: {
+    activeIndexChange: (swiper) => {
+      expirienceStages[swiper.previousIndex].classList.remove("stage_active");
+      expirienceStages[swiper.activeIndex].classList.add("stage_active");
+    },
+  },
+});
 
-let currentActiveElementId = 0;
-let startClientX;
-let scrollLeft;
-
-function moveElements() {
-  console.log(currentActiveElementId);
-  elements[currentActiveElementId].scrollIntoView({
-    behavior: "smooth",
-    inline: "center",
-  });
-}
-
-function moveToNextElement() {
-  if (currentActiveElementId === elements.length - 1) return;
-  currentActiveElementId++;
-  moveElements();
-}
-
-function moveToPrevElement() {
-  if (currentActiveElementId === 0) return;
-  currentActiveElementId--;
-  moveElements();
-}
-
-function handlePointerDown(event) {
-  startClientX = event.clientX;
-  scrollLeft = elementsContainer.scrollLeft;
-
-  elementsContainer.setPointerCapture(event.pointerId);
-
-  elementsContainer.addEventListener("pointermove", handlePointerMove);
-  elementsContainer.addEventListener("pointerup", handlePointerUp);
-}
-
-function handlePointerMove(event) {
-  const delta = event.clientX - startClientX;
-  elementsContainer.scrollLeft = scrollLeft - delta;
-}
-
-function handlePointerUp(event) {
-  const delta = event.clientX - startClientX;
-  currentActiveElementId = calcCurrentElementAfterScroll(delta);
-  moveElements();
-  startClientX = 0;
-
-  elementsContainer.removeEventListener("pointermove", handlePointerMove);
-  elementsContainer.removeEventListener("pointerup", handlePointerUp);
-}
-
-elementsContainer.addEventListener("pointerdown", handlePointerDown);
-prevButton.addEventListener("click", moveToPrevElement);
-nextButton.addEventListener("click", moveToNextElement);
-
-function calcCurrentElementAfterScroll(delta) {
-  const scrollCenterPosition =
-    elementsContainer.scrollLeft + halfContainerWidth;
-
-  if (scrollCenterPosition < elementsPositions[0][0]) {
-    return 0;
-  }
-
-  if (scrollCenterPosition > elementsPositions[elements.length - 1][0]) {
-    return elements.length - 1;
-  }
-
-  for (
-    let elementIndex = 0;
-    elementIndex < elements.length - 1;
-    elementIndex++
-  ) {
-    if (
-      elementsPositions[elementIndex][0] <= scrollCenterPosition &&
-      scrollCenterPosition < elementsPositions[elementIndex + 1][0]
-    ) {
-      return delta > 0
-        ? Math.max(--elementIndex, 0)
-        : Math.min(++elementIndex, elements.length - 1);
-    }
-  }
+for (let stageIndex = 0; stageIndex < expirienceStages.length; stageIndex++) {
+  expirienceStages[stageIndex].onclick = () => {
+    console.log(stageIndex);
+    swiper2.slideTo(stageIndex);
+  };
 }
