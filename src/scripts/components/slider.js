d0e77d4fbacc85b67be3d45b612224bfe5e538a3
Никита Агафонов <agafonikita@outlook.com>
2023-01-09 21:41:18 +0300

    add target form

--- src/scripts/components/slider.js
+++ src/scripts/components/slider.js
@@ -14,27 +14,43 @@ const swiper = new Swiper(".slider", {
 
 const expirienceStages = document.querySelectorAll(".stage");
 
+const mediaQuery = window.matchMedia("(max-width: 1023px)");
+
 const swiper2 = new Swiper(".experience__slider", {
   spaceBetween: 0,
-  centeredSlides: true,
+  centeredSlides: !mediaQuery.matches,
   slidesPerView: "auto",
   grabCursor: true,
   initialSlide: 2,
   pagination: {
     el: ".experience__slider-pagination",
+    clickable: true,
+    renderBullet: function (_, className) {
+      return `<span class="${className} experience__slider-bullet"></span>`;
+    },
   },
   modules: [Pagination],
   on: {
     activeIndexChange: (swiper) => {
       expirienceStages[swiper.previousIndex].classList.remove("stage_active");
       expirienceStages[swiper.activeIndex].classList.add("stage_active");
+      expirienceStages[swiper.previousIndex].querySelector(
+        ".stage__discrip-wrapper"
+      ).style.height = 0;
+      const height =
+        expirienceStages[swiper.activeIndex].querySelector(
+          ".stage__discrip"
+        ).offsetHeight;
+      console.log(height);
+      expirienceStages[swiper.activeIndex].querySelector(
+        ".stage__discrip-wrapper"
+      ).style.height = height + "px";
     },
   },
 });
 
 for (let stageIndex = 0; stageIndex < expirienceStages.length; stageIndex++) {
   expirienceStages[stageIndex].onclick = () => {
-    console.log(stageIndex);
     swiper2.slideTo(stageIndex);
   };
 }
